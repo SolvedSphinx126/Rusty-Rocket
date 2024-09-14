e153b50c623c872f42582463d1f01dbd08a20ab2
Ben O'Bryan <obryanbw@gmail.com>
2024-09-13 22:55:34 -0500

    move blinking to a task

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,32 @@
 
 mod fmt;
 
+use defmt::unwrap;
 #[cfg(not(feature = "defmt"))]
 use panic_halt as _;
 #[cfg(feature = "defmt")]
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Speed};
 use embassy_time::{Duration, Timer};
 use fmt::info;
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    let mut led = Output::new(p.PB7, Level::High, Speed::Low);
+    let led = p.PB7.degrade();
+    unwrap!(spawner.spawn(blinker(led, Duration::from_millis(100))));
+}
 
+#[embassy_executor::task]
+async fn blinker(led: AnyPin, interval: Duration) {
+    let mut led = Output::new(led, Level::Low, Speed::Low);
+    info!("Blink task start");
     loop {
-        info!("Hello, World!");
         led.set_high();
-        Timer::after(Duration::from_millis(500)).await;
+        Timer::after(interval).await;
         led.set_low();
-        Timer::after(Duration::from_millis(500)).await;
+        Timer::after(interval).await;
     }
 }
